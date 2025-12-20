a4ef0f888a9ed04b8eda84ca04c1745f59d53a55
Shunhankar Singh <whstatus71@gmail.com>
2025-12-20 14:10:54 +0530

    Add optimal solution of Number Complement by Bit Manipulation

--- basics/number_complement.cpp
+++ basics/number_complement.cpp
@@ -1,40 +1,29 @@
 // Problem: 476 - Number Complement
 // Leetcode: https://leetcode.com/problems/number-complement/
 
-// Solution Type: Bit Manipulation / Simulation
-// Approach: Convert the number to binary, flip each bit to form its complement,
-//           then convert the resulting binary string back to an integer.
-// Time Complexity: O(log₂(n))
-// Space Complexity: O(log₂(n))
+// Solution Type: Bit Manipulation
+// Approach:
+//   - We create a mask consisting of all 1s having the same bit-length as the given number.
+//   - The mask is constructed by finding the highest power of 2 that is just greater than `num`,
+//     and then subtracting 1 from it.
+//   - We are masking the given number because of the property that for every binary number,
+//     if we perform an XOR operation with ((highest power of 2 which is just greater than num) - 1),
+//     the result is the complement of that binary number.
+//   - Unsigned integer is used to avoid overflow during left shift operations.
 
-
-#include <string>
-using std::string;
+// Time Complexity: O(log₂ n)
+// Space Complexity: O(1)
 
 class Solution {
 public:
     int findComplement(int num) {
-        if (num < 0)
-            return -1;
-
-        if (num == 0)
-            return 1;
-
-        string complement = "";
-        int bit;
-
-        while (num > 0) {
-            bit = num % 2;
-            //It directly converts the binary to it's complement
-            complement = char('0' + (bit == 0 ? 1 : 0)) + complement;
-            num /= 2;
-        }
-
-        int result = 0;
-        for (char c : complement) {
-            result = result * 2 + (c - '0');
-        }
-
-        return result;
+        
+        unsigned int mask = 1;   // Prevents from overflow and becomes negative value of mask while performing left shift operation 
+        
+        while (mask <= (unsigned int)num)
+            mask <<= 1;
+        
+        mask -= 1;               // Creates a mask with all bits set to 1
+        return mask ^ num;       // XOR with mask gives the complement
     }
 };
